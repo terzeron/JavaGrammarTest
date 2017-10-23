e3462ab56c7f0d620b4f61bbe21772031bf23167
Young-il Cho <youngilcho@nhnent.com>
2017-10-23 21:25:19 +0900

    initial commit

---
+++ src/main/java/net/terzeron/test/virtual_extension_method2/DefenderMethodsResolution.java
@@ -0,0 +1,12 @@
+package net.terzeron.test.virtual_extension_method2;
+
+/**
+ * Created by terzeron on 2017. 10. 23..
+ */
+public class DefenderMethodsResolution {
+    public static void main(String[] args) {
+        MySuperClass obj = new MySuperClass();
+        System.out.println(obj.findProduct(4, 5));
+    }
+}
+
