6cd80fa8a45e374e8ef8318367c9198f72250734
Young-il Cho <youngilcho@nhnent.com>
2018-05-30 20:09:13 +0900

    try with resources 구문 예제 수정

--- src/main/java/net/terzeron/test/try_with_resources/TryWithResourcesTest.java
+++ src/main/java/net/terzeron/test/try_with_resources/TryWithResourcesTest.java
@@ -4,9 +4,11 @@ import java.io.*;
 
 public class TryWithResourcesTest {
     public static void main(String[] args) throws FileNotFoundException {
-        FileReader fr = new FileReader("input.txt");
-        BufferedReader br = new BufferedReader(fr);
-        try (fr; br) {
+
+        try (
+                FileReader fr = new FileReader("input.txt");
+                BufferedReader br = new BufferedReader(fr)
+        ) {
             String line;
             while (null != (line = br.readLine())) {
                 // processing each line of file
