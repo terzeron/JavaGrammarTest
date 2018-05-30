fa16396b0bb13fd922aff18e6360d5779085a65e
Youngil Cho <terzeron@gmail.com>
2018-05-31 00:51:43 +0900

    add @BeforeAll anotation to start server

--- src/main/java/net/terzeron/test/socket/SocketTest.java
+++ src/main/java/net/terzeron/test/socket/SocketTest.java
@@ -1,34 +1,28 @@
 package net.terzeron.test.socket;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SocketTest {
-    private ServerThread t;
-
-    class ServerThread extends Thread {
+    static class ServerThread extends Thread {
         public void run() {
             GreetServer server = new GreetServer();
             server.start(6666);
         }
     }
 
-    void startServer() {
+    private static ServerThread t;
+
+    @BeforeAll
+    static void startServer() {
         t = new ServerThread();
         t.start();
     }
 
     @Test
     public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() {
-        startServer();
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
         GreetClient client = new GreetClient();
         client.startConnection("127.0.0.1", 6666);
 
@@ -37,6 +31,5 @@ public class SocketTest {
 
         response = client.sendMessage("hello there");
         assertEquals("unrecognized greeting", response);
-
     }
 }
