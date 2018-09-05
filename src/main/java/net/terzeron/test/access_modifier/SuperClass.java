e63df2a067b5fc8c4e21a945fa305ed8fd2cbf41
Young-il Cho <youngilcho@nhnent.com>
2018-09-05 17:35:44 +0900

    add an example for access modifier

---
+++ src/main/java/net/terzeron/test/access_modifier/SuperClass.java
@@ -0,0 +1,13 @@
+package net.terzeron.test.access_modifier;
+
+public class SuperClass {
+    protected int i;
+
+    protected SuperClass() {
+        // Private constructor
+    }
+
+    protected void method() {
+        // private method
+    }
+}
