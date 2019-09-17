8f2d506821b6b11cf0dfc0a296e71b060a7b2139
Youngil Cho <terzeron@gmail.com>
2019-09-17 22:40:14 +0900

    add an example code

---
+++ StreamTest/src/main/java/com/terzeron/grammar/lambda/InfiniteStreamTest.java
@@ -0,0 +1,39 @@
+package com.terzeron.grammar.lambda;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InfiniteStreamTest {
+    private static void iterationTest() {
+        // infinite stream by iterate()
+        Stream<Integer> stream1 = Stream.iterate(0, i -> i + 2);
+        List<Integer> list1 = stream1
+                .limit(10)
+                .collect(Collectors.toList());
+        for (Integer i : list1) {
+            System.out.println(i);
+        }
+    }
+
+    private static void customTypeInfiniteStreamTest() {
+        Supplier<UUID> randomUUIDSupplier = UUID::randomUUID;
+        Stream<UUID> infiniteStreamOfRandomUUID = Stream.generate(randomUUIDSupplier);
+        List<UUID> randomIDs = infiniteStreamOfRandomUUID
+                .skip(10)
+                .limit(10)
+                .collect(Collectors.toList());
+        for (UUID id : randomIDs) {
+            System.out.println(id);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("---- iterationTest ----");
+        iterationTest();
+        System.out.println("---- customTypeInfiniteStream ----");
+        customTypeInfiniteStreamTest();
+    }
+}
